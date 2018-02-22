b0081f44a87e5d4a0f3590c5f480c7ffffd48213
K4N0 <bad_cholo@yahoo.com>
2018-02-22 13:38:59 -0700

    commit

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(w, "Hello, Go Web Development")
 	})
 
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.ListenAndServe(":3000", nil)
+
 }
