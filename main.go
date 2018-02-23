b653c5113efd379d5d511af5ab869e59afe7c2e7
K4N0 <bad_cholo@yahoo.com>
2018-02-22 18:13:42 -0700

    commit

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type Page struct {
+	Name     string
+	DBStatus bool
+}
+
 func main() {
-	templates := template.Must(template.ParseFiles("templates/index.html"))
+	templates := template.Must(template.ParseFiles("index.html"))
+
+	db, _ := sql.Open("sqlite3", "test.db")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+		p := Page{Name: "Gopher"}
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+
+		p.DBStatus = db.Ping() == nil
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+
 		fmt.Fprintf(w, "Hello, Go Web Development")
 	})
 
